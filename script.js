23b67da42bfd05c7351de3d815ef523193d8943c
diego-carneio <diegosantos.cs@gmail.com>
2021-11-26 15:58:13 -0300

    server interaction

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
-function selecionar (opcao) {
+const getURL = "https://mock-api.driven.com.br/api/v4/shirts-api/shirts";
+const postURL = "https://mock-api.driven.com.br/api/v4/shirts-api/shirts";
+
+function selecionar(opcao) {
     console.log(opcao)
 
     const vo = opcao.parentNode.parentNode;
     console.log(vo)
 
-    console.log(vo.querySelectorAll(".seleted"))
+    console.log(vo.querySelector(".selected"))
 
-    if (vo.querySelector(".seleted")){
-        const preSelected = vo.querySelector(".seleted")
+    if (vo.querySelector(".selected")) {
+        const preSelected = vo.querySelector(".selected")
         console.log(preSelected)
         preSelected.classList.remove("selected")
     }
 
     opcao.classList.add("selected")
+}
+
+function lastOrdersRequest() {
+
+    const promise = axios.get(getURL);
+    promise.then(response => {
+
+        lastOrdersRendering(response.data)
+    });
+
+}
+lastOrdersRequest();
+
+function lastOrdersRendering(list) {
+
+    const allItens = document.querySelector(".allItens");
+
+    list.forEach(item => {
+
+        allItens.innerHTML += `
+            <div class="orderBox">
+                <img src="${item.image}">
+                <p>${item.owner}</p>
+            </div>
+        `
+    });
+
 }
