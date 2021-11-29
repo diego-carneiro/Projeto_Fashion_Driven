ecdceda49ffd9aab75c04a96d5e80794e4422322
diego-carneio <diegosantos.cs@gmail.com>
2021-11-29 13:55:46 -0300

    order finished

--- script.js
+++ script.js
@@ -1,17 +1,38 @@
 const getURL = "https://mock-api.driven.com.br/api/v4/shirts-api/shirts";
 const postURL = "https://mock-api.driven.com.br/api/v4/shirts-api/shirts";
 let count = 0;
+let modelName = " ";
+let modelHTML = " ";
+let neckName = " ";
+let neckHTML = " ";
+let materialName = " ";
+let materialHTML = " ";
+let imageLink = " ";
+let ownerName = " ";
+let authorName = " ";
+let finishedOrder = " ";
+
 
 function userName() {
 
-    const name = prompt("Entre com o seu nome")
-    console.log(name)
+    authorName = prompt("Entre com o seu nome")
 }
 userName();
 
 function selectModel(option) {
 
     const iconGroup = option.parentNode.parentNode;
+    const getName = option.parentNode;
+    const listChildren = getName.children;
+    modelHTML = (listChildren[1].innerHTML)
+
+    if(modelHTML === "T-shirt"){
+        modelName = "t-shirt";
+    } else if ( modelHTML === "Camiseta"){
+        modelName = "long";
+    } else if (modelHTML === "Manga longa"){
+        modelName = "top-tank";
+    }
 
     if (iconGroup.querySelector(".selected")) {
         const preSelected = iconGroup.querySelector(".selected")
@@ -25,9 +46,20 @@ function selectModel(option) {
     finishOrder()
 
 }
-function selectCollar(option) {
+function selectNeck(option) {
 
     const iconGroup = option.parentNode.parentNode;
+    const getName = option.parentNode;
+    const listChildren = getName.children;
+    neckHTML = (listChildren[1].innerHTML)
+
+    if(neckHTML === "Gola V"){
+        neckName = "v-neck";
+    } else if (neckHTML === "Gola Redonda"){
+        neckName = "round";
+    } else if (neckHTML === "Gola polo"){
+        neckName = "polo";
+    }
 
     if (iconGroup.querySelector(".selected")) {
         const preSelected = iconGroup.querySelector(".selected")
@@ -37,11 +69,23 @@ function selectCollar(option) {
     }
 
     option.classList.add("selected")
+
     finishOrder()
 }
-function selectCloth(option) {
+function selectMaterial(option) {
 
     const iconGroup = option.parentNode.parentNode;
+    const getName = option.parentNode;
+    const listChildren = getName.children;
+    materialHTML = (listChildren[1].innerHTML)
+
+    if(materialHTML === "Seda"){
+        materialName = "silk";
+    } else if (materialHTML === "Algodão"){
+        materialName = "cotton";
+    } else if (materialHTML === "Poliéster"){
+        materialName = "polyester";
+    }
 
     if (iconGroup.querySelector(".selected")) {
         const preSelected = iconGroup.querySelector(".selected")
@@ -53,15 +97,41 @@ function selectCloth(option) {
     option.classList.add("selected")
     finishOrder()
 }
+
 function finishOrder() {
 
     const button = document.querySelector(".confirmOrder")
 
-    if(count === 3){
+    if (count === 3) {
         button.classList.add("buttonEnabled")
     }
+
+    imageLink = document.getElementById("imgLink").value;
+
 }
 finishOrder();
+
+function submitOrder() {
+
+    finishOrder();
+
+    const finishedOrder = {
+        model: `${modelName}`,
+        neck: `${neckName}`,
+        material: `${materialName}`,
+        image: `${imageLink}`,
+        owner: `${authorName}`,
+        author: `${authorName}`
+    }
+    console.log(finishedOrder)
+
+    alert(`Sua encomenta é uma roupa com as seguintes características: -${modelHTML} -${neckHTML} -${materialHTML}`);
+
+    const promise = axios.post(postURL, finishedOrder);
+    promise.then();
+    promise.catch((error) => alert(error + "Ops, não conseguimos processar sua encomenda"));
+    
+}
 
 function lastOrdersRequest() {
 
@@ -87,5 +157,4 @@ function lastOrdersRendering(list) {
             </div>
         `
     });
-
 }
